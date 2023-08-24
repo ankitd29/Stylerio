0fa74623081a40e9f6ce60149fee649d56a4004d
ankitd29 <ankitdighe29@gmail.com>
2023-08-24 23:09:21 +0530

    Creating-react-app

---
+++ fashion/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div >
+      <header className="App-header">
+        <a href = "/">BLISSS TSHIRTS</a>
+      </header>
+      <main>
+        List your products here
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ test.js
@@ -0,0 +1,6 @@
+import React from "react"
+function App(){
+    return <div Classname='test'>hello I am ankit</div>
+}
+
+export default App;
