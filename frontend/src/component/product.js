aeb9f61b4aa8702dc57ee0c0a58b0cb8f4708049
Ankit Dighe <136194305+ankitd29@users.noreply.github.com>
2024-04-26 23:58:18 +0530

    Add files via upload

---
+++ frontend/src/component/product.js
@@ -0,0 +1,56 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import axios from "axios";
+import { useContext } from "react";
+import { Store } from "../Store";
+
+function Product(props) {
+	const { product } = props;
+	const { state, dispatch: ctxDispatch } = useContext(Store);
+	const {
+		cart: { cartItems },
+	} = state;
+
+	const addToCartHandler = async (item) => {
+		const existItem = cartItems.find((x) => x._id === product._id);
+		const quantity = existItem ? existItem.quantity + 1 : 1;
+		const { data } = await axios.get(`/api/products/${item._id}`);
+		if (data.countInstock < quantity) {
+			window.alert(
+				"Sorry, the item you are looking for is out of stock. Please try another option :)"
+			);
+			return;
+		}
+		ctxDispatch({
+			type: "CART_ADD_ITEM",
+			payload: { ...item, quantity },
+		});
+	};
+
+	return (
+		<Card>
+			<Link to={`/product/${product.slug}`}>
+				<img src={product.image} className="card-img-top" alt={product.name} />
+			</Link>
+			<Card.Body>
+				<Link to={`/product/${product.slug}`}>
+					<Card.Title>{product.name}</Card.Title>
+				</Link>
+				<Rating rating={product.rating} numReviews={product.numReviews} />
+				<Card.Text>Rs {product.price}</Card.Text>
+				<Card.Text>{product.category}</Card.Text>
+				{product.countInstock === 0 ? (
+					<Button variant="light" disabled>
+						Out of Stock{" "}
+					</Button>
+				) : (
+					<Button onClick={() => addToCartHandler(product)}>Add to Cart</Button>
+				)}
+			</Card.Body>
+		</Card>
+	);
+}
+
+export default Product;
---
+++ frontend/src/data.js
@@ -0,0 +1,59 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name:'Nike slim shirt',
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg', //679 x 829 px
+            price: 650,
+            countInstock: 10,
+            brand: 'Nike',
+            rating: 4.3,
+            numReviews: 20,
+            description: '100% cotton shirt',
+        },
+        {
+            _id: '2',
+            name:'Adidas fit shirt',
+            slug:'adidas-fit-shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price: 700,
+            countInstock: 15,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: '100% cotton shirt with branded printing',
+        },
+        {
+            _id: '3',
+            name:'Nike slim pants',
+            slug:'nike-slim-pantt',
+            category:'Pants',
+            image:'/images/p3.jpg',
+            price: 450,
+            countInstock: 20,
+            brand: 'Nike',
+            rating: 3.7,
+            numReviews: 15,
+            description: 'Good quality pants',
+        },
+        {
+            _id: '4',
+            name:'Adidas fit pants',
+            slug:'adidas-fit-pants',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price: 550,
+            countInstock: 30,
+            brand: 'Adidas',
+            rating: 4.3,
+            numReviews: 20,
+            description: '100% cotton pants',
+        }
+
+    ]
+};
+
+export default data;
